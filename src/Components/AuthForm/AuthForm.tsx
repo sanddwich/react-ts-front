3dea8fa955300f7442e60e030036f410d6c69ab7
Denis <64834314+sanddwich@users.noreply.github.com>
2023-09-26 20:10:07 +0400

    1231313

--- src/Components/AuthForm/AuthForm.tsx
+++ src/Components/AuthForm/AuthForm.tsx
@@ -2,35 +2,29 @@ import React, {useState} from 'react';
 import './AuthForm.scss';
 import {Button, Container, Form} from "react-bootstrap";
 import AuthRequestInterface from "../../Interfaces/AuthRequestInterface";
+import {useForm} from "react-hook-form";
 
 interface AuthFormInterface {
     buttonClickHandler: (authData: AuthRequestInterface) => void
 }
 
 const AuthForm = (props: AuthFormInterface) => {
-    const [username, setUsername] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-    const [loginMute, setLoginMute] = useState<boolean>(false);
-    const [passwordMute, setPasswordMute] = useState<boolean>(false);
-    const [loginMuteText, setLoginMuteText] = useState<string>("Muted text Text.");
-    const [passwordMuteText, setPasswordMuteText] = useState<string>("Muted text Password.");
+    const {
+        register,
+        handleSubmit,
+        resetField,
+        formState: {errors},
+    } = useForm<AuthRequestInterface>({})
 
-    const buttonClickHandler = ():void => {
+    const minLength: number = 3;
 
+    const resetFields = (): void => {
+        resetField('username')
+        resetField('password')
     }
 
-    const loginKeyUpHandler = ():void => {
-
-    }
-
-    const passwordKeyUpHandler = ():void => {
-
-    }
-
-    const sendValidData = ():void => {
-        props.buttonClickHandler({
-            username, password
-        });
+    const buttonClickHandler = (data: AuthRequestInterface): void => {
+        console.log(data)
     }
 
     return (
@@ -39,26 +33,50 @@ const AuthForm = (props: AuthFormInterface) => {
             <Form className={`AuthForm__form`}>
                 <Form.Group className={`AuthForm__group`} controlId="formBasicText">
                     <Form.Label className={`AuthForm__label`}>Логин</Form.Label>
-                    <Form.Control type="text" placeholder="Логин"/>
-                    {loginMute && (
+                    <Form.Control
+                        type="text"
+                        placeholder="Логин"
+                        {...register('username', {
+                            required: {value: true, message: 'Обязательное поле для заполнения'},
+                            minLength: {
+                                value: minLength,
+                                message: 'Недостаточное кол-во символов. Минимальное: ' + minLength,
+                            },
+                            pattern: {
+                                value: /^[a-zA-Z0-9-_]+$/i,
+                                message: 'Недопустимые символы. Доступн только латинские и цифры',
+                            },
+                        })}
+                    />
+                    {errors.username && (
                         <Form.Text
                             className={`AuthForm__muted text-muted`}
                             // onKeyUp={event => loginKeyUpHandler()}
                         >
-                            {loginMuteText}
+                            {errors.username.message}
                         </Form.Text>
                     )}
                 </Form.Group>
 
                 <Form.Group className={`AuthForm__group`} controlId="formBasicPassword">
                     <Form.Label className={`AuthForm__label`}>Пароль</Form.Label>
-                    <Form.Control type="password" placeholder="Пароль"/>
-                    {passwordMute && (
+                    <Form.Control
+                        type="password"
+                        placeholder="Пароль"
+                        {...register('password', {
+                            required: { value: true, message: 'Обязательное поле для заполнения' },
+                            minLength: {
+                                value: minLength,
+                                message: 'Недостаточное кол-во символов. Минимальное: ' + minLength,
+                            },
+                        })}
+                    />
+                    {errors.password && (
                         <Form.Text
                             className={`AuthForm__muted text-muted`}
                             // onKeyUp={() => passwordKeyUpHandler()}
                         >
-                            {passwordMuteText}
+                            {errors.password.message}
                         </Form.Text>
                     )}
                 </Form.Group>
@@ -67,7 +85,7 @@ const AuthForm = (props: AuthFormInterface) => {
                         className={`AuthForm__button`}
                         variant="light"
                         type="button"
-                        onClick={() => buttonClickHandler()}
+                        onClick={handleSubmit((data) => buttonClickHandler(data))}
                     >
                         Вход
                     </Button>
--- src/Layouts/MainLayout/MainFooter/MainFooter.tsx
+++ src/Layouts/MainLayout/MainFooter/MainFooter.tsx
@@ -1,12 +1,16 @@
-import { Container } from "react-bootstrap";
+import {Badge, Container} from "react-bootstrap";
 import "./MainFooter.scss";
 
 interface MainFooterInterface {}
 
 const MainFooter = (props: MainFooterInterface) => {
   return (
-    <Container fluid className="MainFooter p-0">
-      <h1>MainFooter</h1>
+    <Container fluid className="MainFooter p-0 pb-2 d-flex justify-content-end">
+        <div>
+            <h4 className={"m-0"}>
+                <Badge bg={'none'} className={"MainFooter__badge"}>@kiselevds</Badge>
+            </h4>
+        </div>
     </Container>
   );
 };
